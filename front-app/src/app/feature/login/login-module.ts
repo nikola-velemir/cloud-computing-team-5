f7e9646eb7150e7975db81f76d5cae0be05c40a6
nikola-velemir <vnexa021@gmail.com>
2025-08-13 12:53:51 +0200

    [init] project theme

---
+++ front-app/src/app/feature/login/login-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginForm } from './component/login-form/login-form';
+
+@NgModule({
+  declarations: [LoginForm],
+  imports: [CommonModule],
+})
+export class LoginModule {}
