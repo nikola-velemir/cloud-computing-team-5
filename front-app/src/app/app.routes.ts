4a46d0338790d3e3e32e20cac3788e60d3a94382
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-08-19 13:13:16 +0200

    feat(login): Add login form component

--- front-app/src/app/app.routes.ts
+++ front-app/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import {
 import {SongView} from './feature/song-view/component/song-view/song-view';
 import {AlbumView} from './feature/album-view/album-view/component/album-view/album-view';
 import {PerformerView} from './feature/performer-view/component/performer-view/performer-view';
+import { LoginForm } from './feature/login/component/login-form/login-form';
 
 export const routes: Routes = [
   {path: 'content-creation', component: ContentCreationForm},
   {path: 'song/:id', component: SongView},
   {path: 'album/:id', component: AlbumView},
   {path: 'performer/:id', component: PerformerView},
+  {path: 'login', component: LoginForm}
 ];
--- front-app/src/app/feature/login/component/login-form/login-form.ts
+++ front-app/src/app/feature/login/component/login-form/login-form.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-login-form',
@@ -6,4 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './login-form.html',
   styleUrl: './login-form.scss',
 })
-export class LoginForm {}
+export class LoginForm implements OnInit {
+  form!: FormGroup;
+  showPassword: boolean = false;
+  waitingResponse = false;
+  redirectUrl!: string;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  async onSubmit() {}
+}
