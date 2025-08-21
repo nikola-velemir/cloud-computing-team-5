f055101857fa0021c80bdf0d58dad09298aabf29
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-08-21 23:03:58 +0200

    feat(nav): Add default components and routes

---
+++ front-app/src/app/feature/admin/component/manage-content/manage-content.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-content',
+  standalone: false,
+  templateUrl: './manage-content.html',
+  styleUrl: './manage-content.scss',
+})
+export class ManageContent {}
--- front-app/src/app/feature/home-page/home-page-module.ts
+++ front-app/src/app/feature/home-page/home-page-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './component/home-page/home-page';
+import { DiscoverPage } from './component/discover-page/discover-page';
 
 @NgModule({
-  declarations: [HomePage],
+  declarations: [HomePage, DiscoverPage],
   imports: [CommonModule],
 })
 export class HomePageModule {}
