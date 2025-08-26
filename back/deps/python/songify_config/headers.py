1f8a945f945e52eed21eded0de90020620a6ad5a
nikola-velemir <vnexa021@gmail.com>
2025-08-26 11:46:58 +0200

    [feature] added a TS cdk project

--- back/deps/python/songify_config/headers.py
+++
@@ -1,5 +0,0 @@
-HEADERS = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",                      # allow all origins
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-}
--- back/src/feature/content-creation/getAlbums/model/album.py
+++
@@ -1,9 +0,0 @@
-
-from dataclasses import dataclass
-
-
-@dataclass
-class AlbumResponse:
-    id:str
-    title:str
-    year:int
