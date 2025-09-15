292063721adf013e8f7ac98a3e0e7bd180fd3783
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-09-15 12:53:53 +0200

    fix(env): change url

--- front-app/src/environments/environement.ts
+++ front-app/src/environments/environement.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://u9xaabyn07.execute-api.eu-central-1.amazonaws.com/prod',
+  apiUrl: 'https://hfanhb2i62.execute-api.eu-central-1.amazonaws.com/prod',
 };
